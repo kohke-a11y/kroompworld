d3f39cd021a15d9343d4c391533d239551b38d72
kohke-a11y <kohke@vcu.edu>
2026-05-07 13:37:47 -0400

    kroomp

---
+++ final/main.js
@@ -0,0 +1,37 @@
+/* $(".dark-btn").click(
+    function(){
+        $(".box").toggleClass("dark");
+        $(".container").toggleClass("dark");
+    }
+); */
+
+
+$(".spin-btn").click(
+        function(){
+            $(".music").toggleClass("spin");
+        }
+)
+/* 
+$(".reveal-btn").click(
+    function(){
+//    $(".chair").css("display", "block");
+//    $(".chair").toggleClass("reveal");      
+      $(".chair").show(); 
+      $(".reveal-btn").hide();
+    }
+) */
+
+//$(".creature").draggable();
+$(".draggable").draggable();
+
+/* 
+
+$("button").click(
+    function(){
+    $("p").toggleClass("main");
+    }
+); 
+
+$ will target every of these p elements
+
+*/
